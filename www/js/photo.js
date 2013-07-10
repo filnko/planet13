23cda2134c15a19ad9db8a547ff1071625025514
Christoph <c@filnko.net>
2013-07-10 23:37:55 +0200

    div

--- www/js/photo.js
+++ www/js/photo.js
@@ -22,16 +22,14 @@ function onPhotoURISuccess(imageURI) {
 
 function capturePhoto() {
   	// Take picture using device camera 
-  	navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 80,
-    destinationType: Camera.DestinationType.FILE_URI,
-    correctOrientation: true });
+  	navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 100,
+    destinationType: Camera.DestinationType.FILE_URI });
 }
 
 function getPhoto() {
   	// Retrieve image file location from photo library
-  	navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 80, 
+  	navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 100, 
     destinationType: Camera.DestinationType.FILE_URI,
-    correctOrientation: true,
     sourceType: Camera.PictureSourceType.PHOTOLIBRARY });
 }
 
