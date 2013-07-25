2ef86e79a8454839f5c9bed52e691a6962a290e9
Christoph <c@filnko.net>
2013-07-25 14:12:07 +0200

    div

--- www/js/feed.js
+++ www/js/feed.js
@@ -48,7 +48,7 @@ $(document).ready(function() {
 		$("#newsheadline", this).text(entries[selectedEntry].title);
 		var contentHTML = "";
 		contentHTML += entries[selectedEntry].content;
-		contentHTML += '<a href="#" onclick="var ref = window.open(\'http://google.com\', \'_system\');">Auf der Homepage lesen</a>';
+		contentHTML += '<a href="#" onclick="var ref = window.open(\''+entries[selectedEntry].link + '\'", \'_system\');">Auf der Homepage lesen</a>';
 		//contentHTML += '<a href="'+entries[selectedEntry].link + '">Auf der Homepage lesen</a>';
 		$("#newscontent",this).html(contentHTML);
 		removeSizes();
